8fd34281baf0bdd05da1591e688855ad78947ce7
Lunatrius <jadran.kotnik@gmail.com>
2014-07-28 23:27:51 +0200

    ctrl+alt+l

--- src/main/java/com/github/lunatrius/test/ExampleMod.java
+++ src/main/java/com/github/lunatrius/test/ExampleMod.java
@@ -1,26 +1,26 @@
-package com.example.examplemod;
+package com.github.lunatrius.test;
 
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.init.Blocks;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
+public class ExampleMod {
     public static final String MODID = "LunaTest";
     public static final String VERSION = "1.0";
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
         EntitySlime slime = new EntitySlime(null);
         slime.setSlimeSize(1); // protected in vanilla
-        for (int i = 0; i < 10; i++) System.out.println("================================================");
+        for (int i = 0; i < 10; i++)
+            System.out.println("================================================");
         System.out.println("Set the slime size to 1! Yay!");
-        for (int i = 0; i < 10; i++) System.out.println("================================================");
+        for (int i = 0; i < 10; i++)
+            System.out.println("================================================");
     }
 }
